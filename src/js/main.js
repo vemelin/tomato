98b43b41363839c918d2eece11272282c764b431
vemelin <vemelin@gamil.com>
2023-03-16 18:31:58 -0500

    first commit starting project portfolio

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+import "../scss/index.scss";
+let count = 0;
+const imp = ['default', 'important', 'so-so']
+document.querySelector('.button-importance').addEventListener('click', ({target}) => {
+  count += 1;
+  if (count >= imp.length) {
+    count = 0
+  }
+
+  for (let i = 0; i < imp.length; i++) {
+    if (count === i) {
+      target.classList.add(imp[i])
+    } else {
+      target.classList.remove(imp[i])
+    }
+  }
+})
